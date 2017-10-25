716a52c78bec743c013dbe451cbf9639cbc9845d
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2017-10-26 10:56:42 +1100

    CHORE updated url in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject datomic-linklist "0.1.0-SNAPSHOT"
+(defproject datomic-linklist "0.1.1-SNAPSHOT"
   :description "An implementation of linked lists for Datomic"
-  :url "https://github.com/pellucidanalytics/datomic-linklist"
+  :url "https://github.com/stevebuik/datomic-linklist"
   :license {:name "Apache License, Version 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.datomic/datomic-free "0.9.4880.6"
                   :exclusions [org.clojure/clojure]]])
